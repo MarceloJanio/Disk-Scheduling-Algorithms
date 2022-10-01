21c9e173bacd80d0a9f3c036794e93e68e4e11f2
MarceloJanio <marcelojanio@ufpi.edu.br>
2022-10-01 16:41:47 -0300

    round robin

--- processos.py
+++ processos.py
@@ -1,3 +1,4 @@
+from concurrent.futures import process
 import sys
 
 def OpenFile():
@@ -27,23 +28,67 @@ for i in range(0,len(save)):
     save[i][0] = int(save[i][0])
     save[i][1] = int(save[i][1])
 
-print(save)
-
 processos = {}
 
 count = 0
 for i in save:
-    processos[count] = {'inicio': i[0], 'tempToExec': i[1], 'finish': False, 'inicioReal': -1, 'finishReal': -1}
+    processos[count] = {'inicio': i[0], 'tempToExec': i[1], 'finish': False, 'inicioReal': -1, 'finishReal': -1, 'momentosExec': [], 'tempRes': i[1]}
     count +=1
 
-countProc = len(processos)
-instante = 0
 
 #processos = sorted(processos, key = processos.get('inicio'))
-procAtual = 0
-def roundRobin():
-    while(countProc!=0):
-        for i in range
-    pass
 
-print(countProc!=0)
+def calculaRetornoMedio(processos):
+    sum = 0
+    for i in range(0, len(processos)):
+        sum += abs(processos[i]['inicio'] - processos[i]['finishReal'])
+    return sum/len(processos)
+
+def calculaRespostaMedio(processos):
+    sum = 0
+    for i in range(0, len(processos)):
+        sum += abs(processos[i]['inicio'] - processos[i]['inicioReal'])
+    return sum/len(processos)
+
+def calculaProntoMedio(processos):
+    sum = 0
+    for i in range(0, len(processos)):
+        tempoExecTotal =  processos[i]['finishReal'] - processos[i]['inicio']
+        for j in processos[i]['momentosExec']:
+            tempoExecTotal -= abs(j[0] - j[1])
+        sum += tempoExecTotal
+    return sum/len(processos)
+
+print(processos)
+
+def roundRobin():
+    countProc = len(processos)
+    procAtual = 0
+    instante = 0
+    while(countProc!=0):
+        quantum = 0
+        if(processos[procAtual]['inicio'] <= instante) and processos[procAtual]['tempRes'] > 0:
+            initialinsta = instante
+            if(processos[procAtual]['inicioReal'] == -1):
+                processos[procAtual]['inicioReal'] = instante
+            canexec = True
+            while(quantum != 2 and canexec):
+                processos[procAtual]['tempRes'] = processos[procAtual]['tempRes'] - 1
+                if(processos[procAtual]['tempRes'] == 0):
+                    instante += 1
+                    canexec = False
+                    processos[procAtual]['finish'] = True
+                    processos[procAtual]['finishReal'] = instante
+                    countProc -= 1
+                else:
+                    instante += 1
+                quantum += 1
+            finalinsta = instante
+            processos[procAtual]['momentosExec'].append([initialinsta, finalinsta])
+        #else:
+         #   instante += 1
+        procAtual += 1
+        if(procAtual) > len(processos)-1:
+            procAtual = 0
+    print("RR %.2f %.2f %.2f" %(calculaRetornoMedio(processos), calculaRespostaMedio(processos), calculaProntoMedio(processos)))
+roundRobin()
